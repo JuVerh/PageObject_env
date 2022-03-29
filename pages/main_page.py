832f602670c9ac89043bbed9546631c3fcff9270
Yuliia Verkhova <yverkhovaya@gmail.com>
2022-03-29 17:42:45 +0300

    Add LoginPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,13 @@
 from .base_page import BasePage   # импорт базового класса
-from selenium.webdriver.common.by import By
-
+from .locators import MainPageLocators
 
 class MainPage(BasePage):  # класс MainPage наследуется от BasePage - будет иметь доступ ко всем его атрибутам и методам
     def go_to_login_page(self):  # вместо browser(мы его передаем и сохраняем на этапе создания Page Object)
         # укажем аргумент self, чтобы иметь доступ к атрибутам и методам класса
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         # обращаемся с self тк браузер хранится как аргумент класса BasePage
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        # * - указывает что передали пару, и этот кортеж нужно распаковать.
